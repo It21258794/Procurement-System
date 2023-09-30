aca9c4487ad01cafa5cdb435e352713b8e02c88e
Shamry Shiraz <IT21277054@my.sliit.lk>
2023-09-30 12:55:51 +0530

    Created order endpoint skeletons

---
+++ Server/src/controller/order.controller.ts
@@ -0,0 +1,14 @@
+export async function createOrder(req: Request, res: Response) {
+  try {
+  } catch {}
+}
+
+export async function getOrderDetailsById(req: Request, res: Response) {
+  try {
+  } catch {}
+}
+
+export async function getOrderList(req: Request, res: Response) {
+  try {
+  } catch {}
+}
