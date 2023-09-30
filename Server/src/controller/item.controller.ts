78b4a97679efa1a760863a056d04d7fe2a9d7c2c
Zimma <heshanth@codelantic.com>
2023-09-30 22:41:48 +0530

    Item crud completed

--- Server/src/controller/item.controller.ts
+++ Server/src/controller/item.controller.ts
@@ -13,4 +13,43 @@ const insertItem = async (req: Request, res: Response) => {
     }
   };
 
-  export default { insertItem };
+
+  const findItemsByName = async (req: Request, res: Response) => {
+    try {
+      const itemName = req.params.itemname as string;
+      const items = await itemService.findItemsByName(itemName);
+      res.status(200).json(items);
+    } catch (err: any) {
+      res.status(400).json({ err: err.message }); 
+    }
+  };
+
+
+  const updateItem = async (req: Request, res: Response) => {
+    try {
+      const itemId = req.params.itemId; 
+      const updatedData = req.body;
+      const updatedItem = await itemService.updateItem(itemId, updatedData);
+      res.status(200).json(updatedItem);
+    } catch (err: any) {
+      res.status(400).json({ err: err.message });
+    }
+  };
+  
+
+  const deleteItem = async (req: Request, res: Response) => {
+    try {
+      const itemId = req.params.itemId; 
+      const isDeleted = await itemService.deleteItem(itemId);
+      if (isDeleted) {
+        res.status(200).json({ message: 'Item deleted successfully' });
+      } else {
+        res.status(404).json({ message: 'Item not found' });
+      }
+    } catch (err: any) {
+      res.status(400).json({ err: err.message });
+    }
+  };
+
+
+  export default { insertItem,updateItem,deleteItem,findItemsByName};
--- Server/src/services/item.service.ts
+++ Server/src/services/item.service.ts
@@ -22,4 +22,42 @@ async function insertItem(dto: any): Promise<any>{
     }
 }
 
-export default { insertItem };
+async function findItemsByName(itemname: string): Promise<any[]> {
+  try {
+    console.log("Searching for item with name:", itemname);
+    const items = await itemmodel.find({ itemname: itemname });
+    console.log("Found items:", items);
+    return items;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function updateItem(itemId: string, updatedData: any): Promise<any> {
+  try {
+    const updatedItem = await itemmodel.findByIdAndUpdate(itemId, updatedData, { new: true });
+
+    if (!updatedItem) {
+      throw new Error('Item not found');
+    }
+    return updatedItem;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function deleteItem(itemId: string): Promise<boolean> {
+  try {
+    const deletedItem = await itemmodel.findByIdAndDelete(itemId);
+
+    if (!deletedItem) {
+      throw new Error('Item not found');
+    }
+
+    return true;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export default { insertItem,findItemsByName,updateItem,deleteItem};
