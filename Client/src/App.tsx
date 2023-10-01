7d860d3c4511a5f05217792b11d3a24dc180d303
kaveesha karunasena <kaveeshalankeshwara2001@gmail.com>
2023-10-01 19:24:19 +0530

    manager component

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,15 +1,19 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import ManagerDashboard from './components/procurementManager/managerDashboard/managerDashboard';
-import SiteList from './components/procurementManager/pages/siteList';
 import OrderList from './components/procurementManager/pages/orderList';
+import OrderTable from './components/procurementManager/pages/OrderTable';
+import SiteList from './components/procurementManager/pages/siteList';
+import Checkout from './components/procurementManager/pages/checkOut';
 
 function ProcurementManagerRoute() {
   return (
     <ManagerDashboard>
       <Routes>
         <Route path="/sites" element={<SiteList />} />
-        <Route path="/order" element={<OrderList />} />
+        <Route path="/orders" element={<OrderList />} />
+        <Route path="/order" element={<OrderTable />} />
+        <Route path="/payment" element={<Checkout />} />
       </Routes>
     </ManagerDashboard>
   );
